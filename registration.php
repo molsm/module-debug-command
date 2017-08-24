0c62d872c1954c074f32c4f067d7c4700c20d53a
Maris Mols <maris9229@gmail.com>
2017-08-24 12:45:06 +0300

    Module rename

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-   'MolsM_DebugCommand',
+   'MolsM_Playground',
    __DIR__
 );
